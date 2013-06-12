3c150ae1b2f603eaaff0661c1b9548fd00b9a43d
Alan Palazzolo <apalazzolo@minnpost.com>
2013-06-12 15:51:53 -0500

    Starting things.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ minnpost-mn-cartograms
 ======================
 
 Cartograms of Minnesota
+
+## Data
+
+* Population data from http://www.gis.leg.mn/metadata/county2010.htm
+
+## Data processing
+
+* ogr2ogr -f GeoJSON -t_srs EPSG:4326 ./data/mn-county2010.geo.json ./data/mn-county2010.shp/county2010.shp
+* topojson -o ./data/mn-county2010.topo.json ./data/mn-county2010.geo.json
---
+++ js/app.js
@@ -0,0 +1,55 @@
+/**
+ *
+ */
+
+(function($, undefined) {
+
+  var map = d3.select('#mn-map').append('svg')
+    .attr('width', 400)
+    .attr('height', 400);
+  
+  var zoom = d3.behavior.zoom()
+    .translate([-38, 32])
+    .scale(.94)
+    .scaleExtent([0.5, 10.0])
+    .on('zoom', updateZoom);
+    
+  var layer = map.append('g')
+    .attr('id', 'layer');
+    
+  var counties = layer.append('g')
+    .attr('id', 'counties')
+    .selectAll('path');
+  
+  updateZoom();
+
+  function updateZoom() {
+    var scale = zoom.scale();
+    layer.attr('transform',
+      'translate(' + zoom.translate() + ') ' +
+      'scale(' + [scale, scale] + ')');
+  }
+    
+  var proj = d3.geo.albersUsa();
+  carto = d3.cartogram()
+    .projection(proj)
+    .value(function(d) {
+      //console.log(d);
+      //return d.POPULATION;
+      return Math.random() * 100;
+    });
+  
+  // Get data
+  d3.json('./data/mn-county2010.topo.json', function(topo) {
+    console.log(topo);
+  
+    var features = carto(topo, topo.objects['mn-county2010.geo'].geometries);
+    console.log(features);
+    layer.selectAll('path')
+      .data(features)
+      .enter()
+      .append('path')
+        .attr('d', carto.path);
+  });
+  
+})(jQuery);
